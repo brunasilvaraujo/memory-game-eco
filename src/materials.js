e08e6f54e925d520230bf67e4478edc7d39327d0
root <bruna-nascimento_@hotmail.com>
2025-05-11 14:54:32 -0300

    feat: adicionando a lista dos emojis e suas categorias

---
+++ src/materials.js
@@ -0,0 +1,84 @@
+const materials = [
+  {
+    id: 1, 
+    name: '🥦',
+    type: 'organic'
+  },
+  {
+    id: 2,
+    name: '🥦',
+    type: 'organic'
+  },
+  {
+    id: 3,
+    name: '🍪',
+    type: 'organic'
+  },
+  {
+    id: 4,
+    name: '🍪',
+    type: 'organic'
+  },
+  {
+    id: 5,
+    name: '📰',
+    type: 'paper'
+  },
+  {
+    id: 6,
+    name: '📰',
+    type: 'paper'
+  },
+  {
+    id: 7,
+    name: '📖',
+    type: 'paper'
+  },
+  {
+    id: 8,
+    name: '📖',
+    type: 'paper'
+  },
+  {
+    id: 9,
+    name: '🖇️',
+    type: 'metal'
+  },
+  {
+    id: 10,
+    name: '🖇️',
+    type: 'metal'
+  },
+  {
+    id: 11,
+    name: '🥫',
+    type: 'metal'
+  },
+  {
+    id: 12,
+    name: '🥫',
+    type: 'metal'
+  },
+  {
+    id: 13,
+    name: '🥛',
+    type: 'glass'
+  },
+  {
+    id: 14,
+    name: '🥛',
+    type: 'glass'
+  },
+  {
+    id: 15,
+    name: '🍾',
+    type: 'glass'
+  },
+  {
+    id: 16,
+    name: '🍾',
+    type: 'glass'
+  },
+];
+
+export default materials;
